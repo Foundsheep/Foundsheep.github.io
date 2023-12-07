caa8417521f96c1c8fb7bf760be62fc9eded6708
Foundsheep <moondeokjong@gmail.com>
2023-12-07 22:01:44 +0900

    [cv.js] image added at top

--- pages/cv.js
+++ pages/cv.js
@@ -1,8 +1,17 @@
 import Link from "next/link";
+import Image from 'next/image';
 
 export default function CV() {
   return (
     <>
+      <div>
+        <Image
+          src='/images/reward.png'
+          height={200}
+          width={300}
+          alt='reward'
+        />
+      </div>
       <Link href="/">Back to Home</Link>
     </>
   )

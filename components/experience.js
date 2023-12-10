1f72f471e54f8fe895cbc4d066d4da7946b822f4
Foundsheep <moondeokjong@gmail.com>
2023-12-10 14:15:42 +0900

    [Added] experience examples

---
+++ components/experience.js
@@ -0,0 +1,8 @@
+export default function Experience() {
+  return (
+    <section>
+      <h3>experience example</h3>
+      <br />
+    </section>
+  )
+}
--- pages/cv.js
+++ pages/cv.js
@@ -1,10 +1,12 @@
 import Link from "next/link";
 import Image from 'next/image';
+import Experience from "../components/experience";
 
 export default function CV() {
   return (
     <>
       <div>
+        <h1>이런 상 받아보신 적 있나요?</h1>
         <Image
           src='/images/reward.png'
           height={200}
@@ -12,6 +14,9 @@ export default function CV() {
           alt='reward'
         />
       </div>
+      <Experience />
+      <Experience />
+      <Experience />
       <Link href="/">Back to Home</Link>
     </>
   )

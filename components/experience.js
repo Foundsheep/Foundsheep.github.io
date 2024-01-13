5d6ea6297f55cd6869818f69ad19add418e2daeb
Foundsheep <moondeokjong@gmail.com>
2024-01-13 23:06:09 +0900

    [experience] project details added

--- components/experience.js
+++ components/experience.js
@@ -1,17 +1,25 @@
-export default function Experience(date) {
-  const array = ["AAA", "BBB", "CCC"]
-  
+export default function Experience({props}) {  
+  const projectBlock = props.projects?.map(({projectTitle, actions}) =>
+    <div>
+      <h3>{projectTitle}</h3>
+      <ul>
+        {actions.map((line) =>
+          <li>{line}</li>
+        )}
+      </ul>
+    </div>
+  )
+
   return (
     <>
       <div>
-        <div>date</div>
+        <div>{props.date}</div>
       </div>
       <div>
-        <ul>
-          {array.map((line) => (
-            <li>{line}</li>
-          ))}
-        </ul>
+        <div>
+          <h2>{props.title}</h2>
+          <div>{projectBlock}</div>
+        </div>
       </div>
     </>
   )
--- pages/cv.js
+++ pages/cv.js
@@ -20,7 +20,30 @@ export default function CV() {
           {
             date: "2022-07 ~ present", 
             title: "PublicAI",
-            info: ["MDM Project", "NDFaas", "한고원"]
+            projects: [
+              {
+                projectTitle:"MDM Project",
+                actions: [
+                  "국내 학교에 보급 예정인 Mobile Device Management 솔루션 구축 내 백엔드 및 보조 PL 담당",
+                  "Android Management API, Firebase Cloud Messaging, Flask를 활용한 안드로이드 MDM 파트 백엔드 구축",
+                  "Django, Django-Rest-Framework를 활용한 웹 콘솔 백엔드 구축",
+                  "API 설계",
+                  "C# 기반 윈도우 MDM 기능 검증",
+                  "React 기반 프론트엔드 개발자와 협업을 위한 토이 프로젝트 진행(개인)",
+                  "C#, AWS, Docker, Python, Flask, Django, Django-Rest-Framework, Postgresql, Nginx, FCM"
+                ]
+              },
+              {
+                projectTitle:"NDFaas",
+                actions: [
+                  "기사 : https://www.lawtimes.co.kr/news/193126",
+                  "범죄자의 모바일에서 추출된 Sqlite DB 기록을 수사에 사용되는 통합된 DB에 적재",
+                  "대규모 SI 프로젝트를 참여를 통해 솔루션 구축의 다방면을 경험",
+                  "단기간 내 새로운 기술 습득 및 구현 경험",
+                  "Java, Spring, Python, Vertica DB, Sqlalchemy"
+                ]
+              }
+            ]
           }
         }
       />

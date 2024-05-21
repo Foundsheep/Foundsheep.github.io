cb54ab3e15b84f7b16652eef7f1a1ccbc1151eee
Foundsheep <moondeokjong@gmail.com>
2024-05-21 14:08:31 +0900

    feat: profile image to be at the different line from text links in 'menu'

--- components/menu.js
+++ components/menu.js
@@ -5,18 +5,24 @@ import Image from "next/image";
 export default function Menu() {
   return (
     <>
-      <nav>
-        <Image
-          priority
-          src='/images/profile_crop.jpg'
-          className={menuStyle.menuImage}
-          height={100}
-          width={100}
-          alt="profile"
-        />
-        <Link href={"/"} className={menuStyle.menuItem}>Home</Link>
-        <Link href={"/cv"} className={menuStyle.menuItem}>CV</Link>
-        <Link href={"/contact"} className={menuStyle.menuItem}>Contact</Link>
+      <nav className={menuStyle.centeredContainer}>
+        <div className={menuStyle.imageContainer}>
+          <Link href={"/"}>
+            <Image
+              priority
+              src='/images/profile_crop.jpg'
+              className={menuStyle.menuImage}
+              height={150}
+              width={150}
+              alt="profile"
+            />
+          </Link>
+        </div>
+        <div className={menuStyle.linkContainer}>
+          <Link href={"/"} className={menuStyle.menuItem}>Home</Link>
+          <Link href={"/cv"} className={menuStyle.menuItem}>CV</Link>
+          <Link href={"/contact"} className={menuStyle.menuItem}>Contact</Link>
+        </div>
       </nav>
     </>
   );

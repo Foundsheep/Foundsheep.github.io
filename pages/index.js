1441927d67593a31f3a935651abf0cc9fdcfbb9b
Foundsheep <moondeokjong@gmail.com>
2024-07-15 10:01:49 +0900

    add: few lines for the philosophy

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,16 @@ export default function Home() {
           <p>
             Hi I'm DJ Moon
           </p>
+          <p>
+            I belive in the philsophy described down below
+            <ul>
+              <li>Better engineers are made with small habits</li>
+                <ul>
+                  <li>Search for and understand the depth of concepts newly encountered</li>
+                </ul>
+              <li>Take responsibilities not only for me, but also for the whole society</li>
+            </ul>
+          </p>
         </div>
     </>
   )

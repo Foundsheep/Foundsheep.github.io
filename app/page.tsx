ddad1eccd7251af1243df54dbe5dc8a01fab0563
Foundsheep <moondeokjong@gmail.com>
2024-08-30 13:27:12 +0900

    add: global css for h1 and p class

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return <>
     <h1>Hello, I'm DJ Moon</h1>
-    <p>Everyday is a great day and deserves to be enjoyed fully by us(at least I believe so).</p>
+    <p>Thanks for visiting my website :)
+      I believe everyday is a great day and deserves to be enjoyed fully by all of us! 
+      One of the reasons I live for is to learn how to be happy 
+      so that I could pass that wisdom on to my children.</p>
+    <p>Below are my philosophy as a developer</p>
   </>
 }

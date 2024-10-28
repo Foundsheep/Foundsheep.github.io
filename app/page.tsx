5ec15c80162b433d7ce53fdf7a4a4ce33680e636
Foundsheep <moondeokjong@gmail.com>
2024-10-28 14:56:18 +0900

    add: introduction

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return <>
     <h1>Hello, I'm DJ Moon</h1>
     <p>
+      For anyone who knows of Harry Potter, I'm like Prof. Severus Snape. I'm quite a cynical person and by appearance, I seem to not trust almost anyone else. However, the advices and thoughts I give to discussions tend to be relativly trustworthy and of sincere thoughts.
+      To help Harry, he eventually loses his life like Prof. Albus Dumbledore. They both did their job to make the ultimate good win, but in a different way. I think Severus would have become more like Albus, if he continued his life after the final win. I hope I'll be oneday like Albus and have a more heart-warming charcter
+    </p>
+    <p>
       To know the Creator of the universe and to live according to His original scheme, which I believe is to enjoy this life in harmony with others, is one of the greatest goals I have for my daily life :).
       Also, below is the list of my philosophy as a developer I've got for the tasks and how I treat them.
     </p>

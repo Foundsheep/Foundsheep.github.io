82a90308bccf489d6fea73bb6f1a28cb7036d4ef
Foundsheep <moondeokjong@gmail.com>
2024-12-13 16:17:04 +0900

    fix: introduction in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return <>
     <h3>Hello, I'm DeokJong(a.k.a DJ) Moon. <br />
-      Let me tell you about myself with a bit of storytelling.</h3>
+      Let me tell you about myself with a short analogy.</h3>
     <p>
       For anyone who knows of Harry Potter, I'm like Prof. Severus Snape. I often give critical opinions and seem very sharp and interested in nothing but work(no worries about hurting anyone, since I hate it). At the end of the story, Severus eventually loses his life to help Harry as Prof. Albus Dumbledore does. I interpret this as both advocates accomplishing their tasks to achieve the ultimate victory in respectively different ways.
     </p>
     <p>
-      Likewise, I serve my organisation with a sense of great magnitude and try to be a member of trust despite being seen as cynical. I think Severus would have become more generous and warm-hearted like Albus if he remained in his life and matured by an unexpected love encountered in the future after the victory. I dream of becoming like Albus one day, generously embracing and leading people under my care, and the warmth of love sprouting from within me making good fruits in my organisation :)
+      Likewise, I serve my organisation with a sense of great magnitude and try to be a member of trust despite being seen as cynical. I think Severus would have become more generous and warm-hearted like Albus if he remained in his life and matured by an unexpected love encountered in the future after the victory. I dream of becoming like Albus one day, generously embracing and leading people under my care, and the warmth of love sprouting from within me to produce good fruits in my organisation :)
     </p>
     {/* <p>
       To know the Creator of the universe and to live according to His original scheme, which I believe is to enjoy this life in harmony with others, is one of the greatest goals I have for my daily life :).

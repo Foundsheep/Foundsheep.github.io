7c6aeac13b99f22ba0149d7b601b9d5d9b887c44
Foundsheep <moondeokjong@gmail.com>
2025-10-29 11:01:08 +0900

    Update the philosophy

--- app/philosophy/page.tsx
+++ app/philosophy/page.tsx
@@ -14,11 +14,11 @@ export default function Philosophy() {
       <li>Keep things simple</li>
       <li>Try to understand the underlying mathematics, but remember its intuition</li>
       <li>Always verify what the colleages say, who tend to go to GPT first and have long conversations with it</li>
-      <li>Not GPT first, but documentation first</li>
+      {/* <li>Not GPT first, but documentation first</li> */}
       <li>Not about speed, but direction</li>
       <li>Try to understand the philosophy of tools and what's under the hood</li>
-      <li>No hassle to show off for career such as copied-and-pasted blogs.</li>
-      <li>No matter what, client's needs should be priortised the most</li>
+      {/* <li>No hassle to show off for career such as copied-and-pasted blogs.</li> */}
+      {/* <li>No matter what, client's needs should be priortised the most</li> */}
     </ol>
     
     <h4 className={headingStyle}>Those regarding 'life'</h4>
@@ -26,6 +26,7 @@ export default function Philosophy() {
       <li>Be humble and remember the merciful Creator</li>
       {/* <li>Learn how to confront the real you. Otherwise, the One you would think you had met would be in disguise too</li> */}
       <li>Don't lose joy at all costs. Otherwise, your life would turn into a miserable loop</li>
+      <li>Be thrilled by your every little success.</li>
       <li>Every day has its own purpose. Being still able to breathe means it</li>
       <li>The longer you live, the more you need to discard from your life to avoid being overwhelmed by it</li>
     </ol>

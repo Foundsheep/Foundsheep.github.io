9ec27f4bd135dc9b35b3153c0975014d3fce890d
Foundsheep <moondeokjong@gmail.com>
2025-12-01 18:20:13 +0900

    Update IntroText

--- components/cv/introText.tsx
+++ components/cv/introText.tsx
@@ -3,6 +3,7 @@ export function IntroText() {
     <div>
       <ul className="list-disc list-inside mt-10">
         <li>Current <strong>MSc Data Science</strong> student at the <strong>University of St Andrews</strong>(<a className="text-rose-400" href="https://www.theguardian.com/education/ng-interactive/2024/sep/07/the-guardian-university-guide-2025-the-rankings" target="_blank">ranked No 2. in the UK</a>)</li>
+        <li>Strong interest in maths behind DS / ML</li>
         <li>Contribute to open-source projects, such as <a className="text-rose-400" href="https://huggingface.co/docs/diffusers/en/index" target="_blank">Huggingface Diffusers</a>, when facing issues myself. The merged PR is used in the <strong>Hyundai Motors</strong> project's customised diffusion model</li>
       </ul>
     </div>
